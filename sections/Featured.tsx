6208dbf930696519b2bae3cd880d1dc51e312247
Giuliano <giuliano@madesa.com>
2023-03-18 16:26:02 -0300

    new component, Featured Images

--- sections/Featured.tsx
+++ sections/Featured.tsx
@@ -13,7 +13,7 @@ export interface Props {
 export default function Featured({images}: Props) {
     return (
         <section class="flex" style="padding: 0px 1rem; margin: 1.5rem 0px 2.5rem;">
-            <div class="flex flex-col gap-2 md:flex-row">
+            <div class="flex flex-col gap-2 md:flex-row md:justify-between md:max-w-[75.25rem]">
                 {images.map((image) => {
                     return (
                         <div>
